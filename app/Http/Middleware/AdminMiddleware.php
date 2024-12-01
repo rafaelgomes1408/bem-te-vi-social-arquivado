c93fd8330ecfc9929f355315fd7c6109866258be
rafaelgomes1408 <rafaelgomes1408@gmail.com>
2024-12-01 15:01:24 -0300

    Adicionado o Middleware para garantir que apenas usuários administradores possam acessar rotas administrativas.

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+// app/Http/Middleware/AdminMiddleware.php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        return redirect('/home')->with('error', 'Acesso negado. Somente administradores podem acessar esta área.');
+    }
+}
